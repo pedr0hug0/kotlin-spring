e6c7095185cdad857bb80ea71ca734e68e7fd55f
Pedro França <pedr0hug0@yahoo.com.br>
2021-11-17 13:32:21 -0300

    Ambientação básica

---
+++ src/main/kotlin/com/example/kotlinspringdemo01/KotlinSpringDemo01Application.kt
@@ -0,0 +1,56 @@
+package com.example.kotlinspringdemo01
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.*
+
+val animais = mutableListOf<Animal>()
+
+@SpringBootApplication
+class KotlinSpringDemo01Application
+
+fun main(args: Array<String>) {
+//	animais.add(Animal("Rex", Especie.CACHORRO, 10.5))
+	runApplication<KotlinSpringDemo01Application>(*args)
+}
+
+@RestController
+@RequestMapping("hello")
+class HelloWorldController {
+
+	@GetMapping
+	fun hello(): String {
+		return "Hello World Implantado"
+	}
+
+	@GetMapping("2")
+	fun helloWorld2(): String {
+		return "Olá mundo BR"
+	}
+}
+
+enum class Especie { CACHORRO, GATO, PASSARO }
+
+data class Animal(val nome: String, val especie: Especie, val peso: Double)
+
+@RestController
+@RequestMapping("animais")
+class AnimalController {
+
+	@GetMapping
+	fun index() = animais
+
+	@PostMapping
+	fun create(@RequestBody animal: Animal): Animal {
+		animais.add(animal)
+		return animal
+	}
+}
+
+
+
+
+
+
+
+
---
+++ src/test/kotlin/com/example/kotlinspringdemo01/KotlinSpringDemo01ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinspringdemo01
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringDemo01ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
